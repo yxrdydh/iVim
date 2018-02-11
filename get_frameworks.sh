f7f970616e4c871d995cb1d88684e99dd577039a
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-11 16:35:20 +0100

    Replaced BASH_SOURCE with PWD

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -3,23 +3,24 @@
 IOS_SYSTEM_VER="1.0"
 HHROOT="https://github.com/holzschu"
 
-(cd "${BASH_SOURCE%/*}/Frameworks"
+
+(cd "${PWD}/Frameworks"
 # ios_system
 echo "Downloading ios_system.framework and associated dylibs"
 curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/release.tar.gz
-( tar -xzf ios_system.framework.tar.gz && rm ios_system.framework.tar.gz && mv release/* . ) || { echo "ios_system failed to download"; exit 1; }
+( tar -xzf release.tar.gz --strip 1 && rm release.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
 )
 
 # We need the sources for Python and Lua, for the headers
 # included by if_lua and if_python.
 ( # Python_ios
-cd "${BASH_SOURCE%/*}/.."
+cd "${PWD}/.."
 git clone https://github.com/holzschu/python_ios
 cd "python_ios"
 sh ./getPackages.sh
 )
 ( # lua_ios 
-cd "${BASH_SOURCE%/*}/.."
+cd "${PWD}/.."
 git clone https://github.com/holzschu/lua_ios
 cd "lua_ios"
 sh ./get_lua_source.sh
